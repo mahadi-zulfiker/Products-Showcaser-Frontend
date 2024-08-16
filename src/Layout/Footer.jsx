3fc6818e124c1172cb0aadcbfbf95e92f6acbb77
mahade <mahade.adib45@gmail.com>
2024-08-16 16:37:17 +0600

    footer added

---
+++ src/Layout/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className="w-full py-8 border-t">
+            <h3 className="text-center">All right reserved by Mahadi Zulfiker | 2024 |</h3>
+        </div>
+    );
+};
+
+export default Footer;
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = () => {
     return (
-        <>
+        <div className="overflow-x-hidden">
             <Header />
             <Outlet />
-        </>
+            <Footer />
+        </div>
     );
 };
 
